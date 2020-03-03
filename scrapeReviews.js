ce8e449c07fc9eecd32c476940b9f9951abed33b
XanderGriff <xander@hubdoc.com>
2020-03-02 21:43:00 -0500

    Finished scrapeReviews.js

--- scrapeReviews.js
+++ scrapeReviews.js
@@ -12,6 +12,22 @@ const fsReadFile = promisify(fs.readFile.bind(fs));
 const REVIEW_URLS_FILENAME = "pitchfork-urls.txt";
 const REVIEW_DATA_FILENAME = "pitchfork-data.csv";
 
+const SELECTORS = {
+  artist: '.single-album-tombstone__artist-links a',
+  albumName: '.single-album-tombstone__review-title',
+  score: '.score',
+  genre: '.genre-list__link',
+  datePosted: '.article-meta--reviews time',
+  author: '.authors-detail__display-name',
+  authorTitle: '.authors-detail__title',
+  labels: '.labels-list li',
+  yearReleased: '.single-album-tombstone__meta-year',
+  reviewBody: '.contents p', 
+  pullQuote: '.review-detail__abstract > p',
+  isBestNewMusic: '.bnm-arrows',
+  isCollection: '.album-picker',
+};
+
 const scrapeReviews = async () => {
   const reviewUrls = await readUrlsFromInputFile();
   await initializeReviewDataFile();
@@ -28,19 +44,18 @@ const convertFromTextToArray = (listOfLinks) => {
 };
 
 const initializeReviewDataFile = async () => {
-  const fileHeader = 'Artist,Album Name,Score,Genre,Date Posted,Author,Author Title,Label,Year Released,Album Art Description,Review Body';
+  const fileHeader = 'ID,Artist,Album Name,Score,Genre,Date Posted,Author,Author Title,Label,Year Released,Pull Quote,Review Body,Word Count,Is Best New Music,Is Collection\n';
   await fsWriteFile(REVIEW_DATA_FILENAME, fileHeader);
 };
 
 const extractDataFromReviewPages = async (listOfLinks) => {
   console.log("starting review extraction");
 
-  for(link in listOfLinks) {
-    console.log(`On review: ${link}`);
-    const reviewPage = await fetchGivenPage(currentPageNum);
-
-    const reviewData = extractDataFromPage(reviewPage);
-    await writeUrlsToFile(reviewData);
+  for(const [index, link] of listOfLinks.entries()) {
+    console.log(`On review ${index}: ${link}`);
+    const reviewPage = await fetchGivenPage(link);
+    const reviewData = extractDataFromPage(reviewPage, index);
+    await writeReviewDataToFile(reviewData);
     await waitASecond();
   }
   console.log("finished url extraction");
@@ -60,29 +75,85 @@ const convertRawTextToDOM = rawPage => {
   return new JSDOM(rawPage);
 };
 
-const extractDataFromPage = page => {
-  // const document = page.document;
+const extractDataFromPage = (page, index) => {
+  const document = page.window.document;
 
-  // const reviewData = {
-  //   artist: document.querySelector(),
-  //   albumName: document.querySelector(),
-  //   score: document.querySelector(),
-  //   genre: document.querySelector(),
-  //   datePosted: formatDate(document.querySelector()),
-  //   author: document.querySelector(),
-  //   authorTitle: document.querySelector(),
-  //   label: document.querySelector(),
-  //   yearReleased: document.querySelector(),
-  //   albumArtDescription: document.querySelector(),
-  //   reviewBody: document.querySelector(),
-  // }
+  const reviewData = {
+    id: index,
+    artist: getTextContentFromDOM(document, SELECTORS.artist),
+    albumName: getTextContentFromDOM(document, SELECTORS.albumName),
+    score: getTextContentFromDOM(document, SELECTORS.score),
+    genre: getTextContentFromDOM(document, SELECTORS.genre),
+    datePosted: getDateTimeFromDOM(document, SELECTORS.datePosted),
+    author: getTextContentFromDOM(document, SELECTORS.author),
+    authorTitle: getTextContentFromDOM(document, SELECTORS.authorTitle),
+    labels: getMultipleFields(document, SELECTORS.labels),
+    yearReleased: getYearReleased(document, SELECTORS.yearReleased),
+    pullQuote: getTextContentFromDOM(document, SELECTORS.pullQuote),
+    reviewBody: getMultipleFields(document, SELECTORS.reviewBody),
+    wordCount: getWordCount(getMultipleFields(document, SELECTORS.reviewBody)),
+    isBestNewMusic: selectorExists(document, SELECTORS.isBestNewMusic),
+    isCollection: selectorExists(document, SELECTORS.isCollection),
+  }
 
-  // return reviewData;
+  return reviewData;
 };
 
-const formatDate = (date) => {
-
+const writeReviewDataToFile = async reviewData => {
+  const dataToWrite = formatReviewDataForWrite(reviewData);
+  await fsAppendFile(REVIEW_DATA_FILENAME, dataToWrite);
 };
+
+const formatReviewDataForWrite = reviewData => {
+  return Object.values(reviewData).join(",") + "\n";
+};
+
+const getTextContentFromDOM = (document, selector) => {
+  if(document && document.querySelector(selector)) {
+    return sanitizeText(document.querySelector(selector).textContent);
+  } else {
+    return null;
+  }
+};
+
+const getDateTimeFromDOM = (document, selector) => {
+  if(document && document.querySelector(selector)) {
+    return sanitizeText(document.querySelector(selector).dateTime);
+  } else {
+    return null;
+  }
+};
+
+const getNodeListFromDOM = (document, selector) => {
+  return document.querySelectorAll(selector);
+};
+
+const getMultipleFields = (document, selector) => {
+  const fields = [];
+
+  const fieldElements = getNodeListFromDOM(document, selector);
+  for(let field of fieldElements) {
+    fields.push(sanitizeText(field.textContent));
+  }
+
+  return fields.join(';');
+};
+
+const getYearReleased = (document, selector) => {
+  return getTextContentFromDOM(document, selector).replace(/[^\d]/g, '');
+}
+
+const getWordCount = (string) => {
+  return string.split(' ').length;
+};
+
+const selectorExists = (document, selector) => {
+  return document.querySelector(selector) !== null;
+};
+
+const sanitizeText = (text) => {
+  return text.replace(/,/g, '&comma');
+}
 
 const waitASecond = async () => {
   await new Promise(resolve => {
